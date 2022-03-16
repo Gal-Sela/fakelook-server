ba17773d74e92fd6eee278d9e417530924b25cc1
Gal <galsela2022@gmail.com>
2022-03-16 17:40:06 +0200

    add filter model, change user model

---
+++ fakeLook-models/Models/Filter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fakeLook_models.Models
+{
+    public class Filter
+    {
+        public ICollection<string> Publishers { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public double Radius { get; set; }
+        public ICollection<string> Tags { get; set; }
+        public ICollection<string> UsersTaggedInPostId { get; set; }
+
+    }
+}
